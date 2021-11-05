198b9c5431c0c6ffbe6f5dd3858ff6ca4e5ac113
Jeff Nyalik <jeffnyak@gmail.com>
2021-11-05 15:38:09 +0300

    register and login design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
 import { AddPropertyComponent } from './components/properties/add-property/add-property.component';
-import { PropertyListComponent } from './components/properties/property-list/property-list.component';
 import { PropertyDetailComponent } from './components/properties/property-detail/property-detail.component';
+import { PropertyListComponent } from './components/properties/property-list/property-list.component';
 import { UserLoginComponent } from './components/user/user-login/user-login.component';
 import { UserRegisterComponent } from './components/user/user-register/user-register.component';
 
 const routes: Routes = [
+  {path: 'home', component: HomeComponent},
   {path: '', component: PropertyListComponent},
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
